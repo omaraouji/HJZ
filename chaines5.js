3f4fbc644102df449ef34514e86ebb7ea759fb88
omar aouji <aoujiaouji8@gmail/com>
2026-09-22 12:40:13 +0000

    ljadid

---
+++ chaines5.js
@@ -0,0 +1,8 @@
+const prompt = require('prompt-sync')();
+
+let a = prompt("Entrez la première chaîne :");
+let b = prompt("Entrez la deuxième chaîne :");
+
+resultat = a + " " + b;
+
+console.log(resultat);
